c29512f562755cd58b04dfaa529d693a32a09408
Bernard Ojengwa <bernardojengwa@gmail.com>
2016-02-23 00:23:23 +0100

    first tests

---
+++ test/sdk.specs.js
@@ -0,0 +1,11 @@
+var expect = require('expect.js');
+
+describe('Array', function() {
+    describe('#indexOf()', function() {
+     it('should return -1 when the value is not present', function() {
+            expect(5).to.be.a('number');
+            expect([]).to.be.an('array');  // works
+            expect([]).to.be.an('object');
+     });
+    });
+});
